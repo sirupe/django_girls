197debe239385711ace6554b9f3ffb95f4b6f8bb
sirupe <sirupe@nate.com>
2019-12-11 10:58:40 +0900

    savepoint

--- writing/urls.py
+++ writing/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 app_name = 'polls'
 urlpatterns = [
-    path('entrance/', views.entrance, name='entrance')
+    path('entrance/', views.entrance, name='entrance'),
+    path('writing/write_form', views.write_form, name='write_form'),
+    path('write', views.write, name='write')
 ]
--- writing/views.py
+++ writing/views.py
@@ -7,3 +7,14 @@ def entrance(request):
     writings = Writing.objects.all().order_by('-uploaded_time')
     context = {'writings': writings}
     return render(request, 'writing/index.html', context)
+
+
+def write_form(request):
+    a = request
+    print('a : ' , a.path)
+    return render(request, 'writing/write_form.html')
+
+
+def write(request):
+    writing = Writing(title=request.title, contents=request.contents)
+    return None
